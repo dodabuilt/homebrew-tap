94c77d87b3a506dda7d360e32191b7396cd68afc
David Medvedev <dmedvedev@interactivebrokers.com>
2026-01-18 09:50:33 -0500

    Add animated-wallpaper cask v1.0.4

---
+++ Casks/animated-wallpaper.rb
@@ -0,0 +1,25 @@
+cask "animated-wallpaper" do
+  version "1.0.4"
+  sha256 :no_check  # Will be updated with actual SHA256
+
+  url "https://github.com/dodabuilt/AnimatedWallpaper/releases/download/v#{version}/AnimatedWallpaper-Installer.pkg"
+  name "Animated Wallpaper"
+  desc "Transform your macOS desktop with animated video and GIF wallpapers"
+  homepage "https://github.com/dodabuilt/AnimatedWallpaper"
+
+  pkg "AnimatedWallpaper-Installer.pkg"
+
+  uninstall pkgutil: "com.davidmedvedev.animatedwallpaper",
+            delete:  "/Applications/AnimatedWallpaper.app"
+
+  zap trash: [
+    "~/Library/Application Support/AnimatedWallpaper",
+    "~/Library/Preferences/com.dash.animatedwallpaper.plist",
+    "~/Library/Caches/com.dash.animatedwallpaper",
+  ]
+
+  caveats <<~EOS
+    After installation, open Animated Wallpaper from your Applications folder.
+    Look for the icon in your menu bar to get started!
+  EOS
+end
---
+++ README.md
@@ -0,0 +1,29 @@
+# Homebrew Tap
+
+This is a Homebrew tap for applications by David Medvedev.
+
+## Installation
+
+```bash
+brew tap dodabuilt/tap
+brew install --cask animated-wallpaper
+```
+
+## Available Casks
+
+| Cask | Description |
+|------|-------------|
+| `animated-wallpaper` | Transform your macOS desktop with animated video and GIF wallpapers |
+
+## Updating
+
+```bash
+brew update
+brew upgrade --cask animated-wallpaper
+```
+
+## Uninstalling
+
+```bash
+brew uninstall --cask animated-wallpaper
+```
